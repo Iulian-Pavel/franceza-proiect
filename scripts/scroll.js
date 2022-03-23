e4d64b21af29a7caa05e81c5ce084380bf4bdc0f
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2022-03-23 22:58:22 +0200

    cant be bothered to use meaningul messages

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,6 +1,6 @@
 const scrollToAnimals = () => {
     const downArrow = document.querySelector('.down-arrow');
-    const animalSection = document.querySelector('.endangered-animals');
+    const animalSection = document.querySelector('.endangered');
     downArrow.addEventListener('click', () => {
         animalSection.scrollIntoView({
             behavior: "smooth"
